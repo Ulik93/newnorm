620e76e5df512db24e972ea47d0b6a2454e6c8c8
beksultan <beksultan.mamashov@gmail.com>
2021-05-08 14:14:03 +0600

    licenses

--- src/pages/License/License.jsx
+++ src/pages/License/License.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
+import React from "react"
 import "./license.css"
-const License= ()=>{
-  return(
-      <div className="license"></div>
-    )
+import SingleLicense from "./SingleLicense"
+import license1 from "./License__img/license1.jpg"
+import license2 from "./License__img/license2.jpg"
+import license3 from "./License__img/license3.jpg"
+import { useState } from "react"
+
+const data = [
+  {
+    id: 1,
+    url: license1,
+    title: "Свидетельство о государственной регистрации web‑системы СБИС",
+    text: "Подтверждает авторские права на систему СБИС",
+  },
+  {
+    id: 2,
+    url: license2,
+
+    title: "Свидетельство о государственной регистрации web‑системы СБИС",
+    text: "Подтверждает авторские права на систему СБИС",
+  },
+  {
+    id: 3,
+    url: license3,
+    title: "Свидетельство о государственной регистрации web‑системы СБИС",
+    text: "Подтверждает авторские права на систему СБИС",
+  },
+]
+
+const License = () => {
+  const [showModal, setShowModal] = useState(false)
+  const [modalImg, setModalImg] = useState(null)
+
+  return (
+    <div className="license__container">
+      <div className="license__header">Лицензии и сертификаты</div>
+      <div className="license__items">
+        {data.map((item) => {
+          return (
+            <SingleLicense
+              showModal={showModal}
+              setShowModal={setShowModal}
+              modalImg={modalImg}
+              setModalImg={setModalImg}
+              key={item.id}
+              {...item}
+            />
+          )
+        })}
+      </div>
+    </div>
+  )
 }
-export default License;
+export default License
