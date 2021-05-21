4bcf851a49c4f5b146f5a438932a5d004c3cbaac
Ulik93 <uliksh@gmail.com>
2021-05-21 17:17:13 +0600

    ok

--- src/pages/api/axios.js
+++ src/pages/api/axios.js
@@ -2,13 +2,8 @@ import axios from "axios"
 
 let point = "https://norma-76317-default-rtdb.firebaseio.com/"
 const instance = axios.create({
-<<<<<<< HEAD
-  baseURL: "https://norma-33eab-default-rtdb.firebaseio.com",
-});
-export default instance;
-=======
+
   baseURL: "https://norma-76317-default-rtdb.firebaseio.com/",
 })
 
 export default instance
->>>>>>> e67926771a3d18f20fa045e3f60f958c52345a42
